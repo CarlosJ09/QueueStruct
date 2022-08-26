efc2543203ce59233a77f1ceb7d562d3260f838a
CarlosJ09 <carlos.ml.jimenezz@gmail.com>
2022-08-26 15:31:46 -0400

    Updating methods

--- main.cpp
+++ main.cpp
@@ -22,13 +22,13 @@ struct Queue
     Queue *next;
 };
 
-void Menu(); // Menu del programa.
-void Enqueue(); // inserta datos en la cola.
+void Menu();                                         // Menu del programa.
+void Enqueue(Queue *&head, Queue *&end, int &n);     // inserta datos en la cola.
 bool EnqueuePrio(Queue *&cola, int prior, int data); // Inserta datos en la cola con prioridad.
-void Dequeue(); // Elimina datos de la cola.
-void Display(); //Despliega todos los elementos de la Cola realizando un Dequeue de toda la estructura.
-void IsEmpty(); // Verifica si la cola esta vacia, de ser asi manda un mensaje avisando.
-void Exit(); // Sale del programa.
+void Dequeue(Queue *&head, Queue *&end, int &n);     // Elimina datos de la cola.
+void Display();                                      // Despliega todos los elementos de la Cola realizando un Dequeue de toda la estructura.
+bool IsEmpty(Queue *&head);                          // Verifica si la cola esta vacia, de ser asi manda un mensaje avisando.
+void Exit();                                         // Sale del programa.
 
 int main()
 {
@@ -47,17 +47,32 @@ void Menu()
         cin >> menu;
         switch (menu)
         {
-        case 1:
-            Enqueue();
+        case 1: // Insertar datos
+            Queue *head = NULL;
+            Queue *end = NULL;
+
+            int n; // Elemento a introducir por el usuario.
+
+            cout << "Digite el numero que desea ingresar: ";
+            cin >> n;
+            Enqueue(head, end, n);
+
             break;
         case 2:
             Queue *cola;
             int prio;
             int elem;
+            cin >> elem;
+
             EnqueuePrio(cola, prio, elem);
             break;
-        case 3:
-            Dequeue();
+        case 3: // Eliminando un elemento.
+            Queue *head;
+            Queue *end;
+
+            int n;
+
+            Dequeue(head, end, n);
             break;
         case 4:
             Exit();
@@ -71,9 +86,25 @@ void Menu()
     }
 }
 
-void Enqueue()
+void Enqueue(Queue *&head, Queue *&end, int &n)
 {
-    cout << "Insertando Elementos" << endl;
+    Queue *nuevo = new Queue();
+
+    nuevo->data = n;
+    nuevo->next = NULL;
+
+    if (IsEmpty(head))
+    {
+        head = nuevo;
+    }
+    else
+    {
+        end->next = nuevo;
+    }
+
+    end = nuevo;
+
+    cout << "Elemento " << n << " insertado correctamente!" << endl;
 }
 
 bool EnqueuePrio(Queue *&cola, int prior, int data)
@@ -108,14 +139,29 @@ bool EnqueuePrio(Queue *&cola, int prior, int data)
     return ip;
 }
 
-void Dequeue()
+void Dequeue(Queue *&head, Queue *&end, int &n)
 {
-    cout << "Eliminado Elementos" << endl;
+    n = head->data;
+    Queue *aux = head;
+
+    if (head == end)
+    {
+        head == NULL;
+        end == NULL;
+    }
+    else
+    {
+        head = head->next;
+    }
+
+    delete aux;
+
+    cout << "El elemento " << n << "ha sido eliminado" << endl;
 }
 
-void EsEmpty()
+bool IsEmpty(Queue *&head)
 {
-    cout << "La cola esta vacia";
+    return (head == NULL) ? true : false;
 }
 
 void Exit()
