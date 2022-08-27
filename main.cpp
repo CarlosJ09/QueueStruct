03c0c98250aace70507a7dfcebdbacd38ee3709d
CarlosJ09 <carlos.ml.jimenezz@gmail.com>
2022-08-27 12:20:47 -0400

    Fixing bugs

--- main.cpp
+++ main.cpp
@@ -22,44 +22,59 @@ struct Queue
     Queue *next;
 };
 
-void Menu();                                         // Menu del programa.
+int menu = 0; // Para manejar el menu
+
+void Menu(); // Menu del programa.
+void validarMenu();
 void Enqueue(Queue *&head, Queue *&end, int &n);     // inserta datos en la cola.
 bool EnqueuePrio(Queue *&cola, int prior, int data); // Inserta datos en la cola con prioridad.
 void Dequeue(Queue *&head, Queue *&end, int &n);     // Elimina datos de la cola.
-void Display();                                      // Despliega todos los elementos de la Cola realizando un Dequeue de toda la estructura.
+void Display(Queue *&head, Queue *&end, int &n);     // Despliega todos los elementos de la Cola realizando un Dequeue de toda la estructura.
 bool IsEmpty(Queue *&head);                          // Verifica si la cola esta vacia, de ser asi manda un mensaje avisando.
 void Exit();                                         // Sale del programa.
 
 int main()
 {
     Menu();
+
     return 0;
 }
 
 void Menu()
 {
-
+    // Frente y Final de la cola
     Queue *head = NULL;
     Queue *end = NULL;
 
-    int dato;
-    int menu = 0;
-    int vExit;
+    int dato; // Elemento que introduce el usuario
 
     while (menu != 5)
     {
-        cout << "Elija una opcion: \n";
+        cout << "Elija una opcion: \n \n";
+
+        // Opciones
+        cout << "1.Insertar" << endl;
+        cout << "2.Insertar con prioridad" << endl;
+        cout << "3.Eliminar un elemento" << endl;
+        cout << "4.Desplegar" << endl;
+        cout << "5.Salir \n"
+             << endl;
+
         cin >> menu;
         switch (menu)
         {
-        case 1: // Insertar datos
+        case 1: // Insertar datos.
             system("clear");
             cout << "Digite el numero que desea ingresar: ";
             cin >> dato;
 
             Enqueue(head, end, dato);
 
+            cout << "\n"
+                 << endl;
+
             break;
+
         case 2: // Insertar datos con prioridad en la cola.
             system("clear");
             int prior;
@@ -73,26 +88,43 @@ void Menu()
 
             EnqueuePrio(head, prior, dato);
             cout << "Elemento " << dato << " insertado en la cola correctamente con prioridad " << prior << endl;
+
+            cout << "\n"
+                 << endl;
             break;
+
         case 3: // Eliminando un elemento.
             system("clear");
             cout << "Presione cualquier tecla para eliminar el elemento en frente de la cola" << endl;
             cin >> dato;
 
             Dequeue(head, end, dato);
+
+            cout << "\n"
+                 << endl;
+
             break;
+
         case 4: // Desplegando la cola.
-            Display();
+            system("clear");
+            Display(head, end, dato);
+
+            cout << "\n"
+                 << endl;
+
             break;
-        case 5:
+
+        case 5: // Salir
             system("clear");
             Exit();
+
+            cout << "\n"
+                 << endl;
+
             break;
-        default:
-            system("clear");
+        default: // Cualquier otra opcion.
             cout << "Por favor elija una opcion valida \n"
                  << endl;
-            cout << "Elija una opcion: \n";
             break;
         }
     }
@@ -168,7 +200,7 @@ void Dequeue(Queue *&head, Queue *&end, int &n)
 
     delete aux;
 
-    cout << "El elemento " << n << "ha sido eliminado" << endl;
+    cout << "El elemento " << n << " ha sido eliminado" << endl;
 }
 
 bool IsEmpty(Queue *&head)
@@ -176,9 +208,27 @@ bool IsEmpty(Queue *&head)
     return (head == NULL) ? true : false;
 }
 
-void Display()
+void Display(Queue *&head, Queue *&end, int &n)
 {
-    cout << "Hola";
+    while (head != NULL)
+    {
+        n = head->data;
+        Queue *aux = head;
+
+        if (head == end)
+        {
+            head == NULL;
+            end == NULL;
+        }
+        else
+        {
+            head = head->next;
+        }
+
+        delete aux;
+
+        cout << "El elemento " << n << " ha sido eliminado" << endl;
+    }
 }
 
 void Exit()
